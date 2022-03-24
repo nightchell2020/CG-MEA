e88b995b4e5dd0336b0220dd00230a439aed3748
Min-jae Kim (linux) <ipis.mjkim@gmail.com>
2022-03-24 12:57:32 +0900

    Removed files from the index (now ignored)

--- datasets/__init__.py
+++
@@ -1,3 +0,0 @@
-# from cau_eeg_dataset import *
-
-# __all__ = ['cau_eeg_dataset', 'pipeline', 'cau_eeg_script', 'utils']
--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .simple_cnn_1d import *
-from .resnet_1d import *
-from .resnet_2d import *
-from .cnn_transformer import *
-from .utils import *
-
-# __all__ = ['simple_cnn_1d', 'resnet_1d', 'resnet_2d', 'cnn_transformer', 'utils']
