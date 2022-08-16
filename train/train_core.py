ba3db77b0ad92ebb0815148a37f74132bdffdf6b
Min-jae Kim <ipis.mjkim@gmail.com>
2022-08-16 13:52:12 +0000

    clip_grad_norm

--- train/train_core.py
+++ train/train_core.py
@@ -43,6 +43,8 @@ def train_multistep(model, loader, preprocess, optimizer, scheduler, config, ste
 
             # backward and update
             loss.backward()
+            if 'clip_grad_norm' in config:
+                torch.nn.utils.clip_grad_norm_(model.parameters(), config['clip_grad_norm'])
             optimizer.step()
             scheduler.step()
 
