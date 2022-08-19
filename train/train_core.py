15c240eb25d4787b4eff6814090cf04bc2704b25
Min-jae Kim <ipis.mjkim@gmail.com>
2022-08-19 23:44:47 +0900

    multi_margin_loss for mixup training

--- train/train_core.py
+++ train/train_core.py
@@ -126,8 +126,13 @@ def train_mixup_multistep(model, loader, preprocess, optimizer, scheduler, amp_s
                     y_oh = lam * y1_oh + (1.0 - lam) * y2_oh
                     s = torch.sigmoid(output)
                     loss = F.binary_cross_entropy_with_logits(output, y_oh)
+                elif config['criterion'] == 'svm':
+                    s = output
+                    loss1 = F.multi_margin_loss(output, y1)
+                    loss2 = F.multi_margin_loss(output, y2)
+                    loss = lam * loss1 + (1 - lam) * loss2
                 else:
-                    raise ValueError("config['criterion'] must be set to one of ['cross-entropy', 'multi-bce']")
+                    raise ValueError("config['criterion'] must be set to one of ['cross-entropy', 'multi-bce', 'svm']")
 
             # backward and update
             if config.get('mixed_precision', False):
